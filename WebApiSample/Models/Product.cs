e72a39c9862c35fe0961518dbdbf5a020eff2d50
V P <vpernicka@gmail.com>
2024-10-18 15:39:44 +0200

    Add MaxLength to model Product

--- WebApiSample/Models/Product.cs
+++ WebApiSample/Models/Product.cs
@@ -1,11 +1,20 @@
-﻿namespace WebApiSample.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiSample.Models
 {
     public class Product
     {
         public required int Id { get; set; }
+
+        [MaxLength(50)]
         public required string Name { get; set; }
+
         public required decimal Price { get; set; }
+
+        [MaxLength(2048)]
         public required string ImgUri { get; set; }
+
+        [MaxLength(400)]
         public string? Description { get; set; }
     }
 }
