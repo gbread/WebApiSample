c8eb0341ae5783bb3d9bf1f41f2727cc7bbc4912
V P <vpernicka@gmail.com>
2024-10-18 14:24:08 +0200

    Update Services init

--- WebApiSample/Program.cs
+++ WebApiSample/Program.cs
@@ -1,4 +1,10 @@
 
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using WebApiSample.BLL;
+using WebApiSample.DAL;
+using WebApiSample.Mappings;
+
 namespace WebApiSample
 {
     public class Program
@@ -6,13 +12,21 @@ namespace WebApiSample
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+            var services = builder.Services;
             // Add services to the container.
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddAutoMapper(typeof(AutoMapperProfile));
+
+            builder.Services.AddScoped<IProductRepository, ProductRepository>();
+            builder.Services.AddScoped<IProductService, ProductService>();
 
             var app = builder.Build();
 
