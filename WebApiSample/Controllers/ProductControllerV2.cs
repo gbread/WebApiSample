027574d9eb4f764f8cd35d6c3be2ce81e0a0c91c
V P <vpernicka@gmail.com>
2024-10-21 17:21:47 +0200

    Fix Get All Products V2 endpoint
    Return relevant pagination info

--- WebApiSample/Controllers/ProductControllerV2.cs
+++ WebApiSample/Controllers/ProductControllerV2.cs
@@ -2,6 +2,7 @@
 using WebApiSample.BLL;
 using WebApiSample.Controllers.DTOs;
 using WebApiSample.Models;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace WebApiSample.Controllers
 {
@@ -16,18 +17,32 @@ namespace WebApiSample.Controllers
             _productService = productService;
         }
 
+        /// <summary>
+        /// Retrieves paginated list of products.
+        /// </summary>
+        /// <param name="page">The page number (starting at 1).</param>
+        /// <param name="pageSize">The number of items per page.</param>
+        /// <returns>A paginated list of product DTOs.</returns>
         [HttpGet]
-        public async Task<ActionResult<PagedListDto<ProductDto>>> GetProducts(int page, int pageSize)
+        [SwaggerResponse(StatusCodes.Status200OK, "Returns paginated products", typeof(PagedListDto<ProductDto>))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, "Invalid pagination parameters")]
+        public async Task<ActionResult<PagedListDto<ProductDto>>> GetProducts([FromQuery] int page = 1, [FromQuery] int pageSize = 10)
         {
+            if (page <= 0 || pageSize <= 0)
+            {
+                return BadRequest("Page and PageSize must be greater than 0.");
+            }
+
             var products = await _productService.GetProductsAsync(page, pageSize);
 
-            return Ok(
-                new PagedListDto<ProductDto>(
-                    products, 
-                    products.TotalItemCount, 
-                    products.PageNumber, 
-                    products.PageSize)
+            var pagedResult = new PagedListDto<ProductDto>(
+                products,
+                products.TotalItemCount,
+                products.PageNumber,
+                products.PageSize
             );
+
+            return Ok(pagedResult);
         }
     }
 }
