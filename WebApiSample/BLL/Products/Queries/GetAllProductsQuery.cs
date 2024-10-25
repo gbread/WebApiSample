415259062ecc47ad78808140ba739ec4c4d3cac2
V P <vpernicka@gmail.com>
2024-10-25 21:11:58 +0200

    Add GetAllProductsQuery to mediator

---
+++ WebApiSample/BLL/Products/Queries/GetAllProductsQuery.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MediatR;
+using WebApiSample.DAL;
+using WebApiSample.Models;
+
+namespace WebApiSample.BLL.Products.Queries
+{
+    public class GetAllProductsQuery : IRequest<List<ProductDto>> { }
+
+    public class GetProductsQueryHandler : IRequestHandler<GetAllProductsQuery, List<ProductDto>>
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+
+        public GetProductsQueryHandler(IProductRepository productRepository, IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<ProductDto>> Handle(GetAllProductsQuery request, CancellationToken cancellationToken)
+        {
+            var products = await _productRepository.GetProductsAsync(cancellationToken);
+            return _mapper.Map<List<ProductDto>>(products);
+        }
+    }
+}
