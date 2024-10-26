3d4372d894e5cba8a3ee2b9d57258c2f2ce341db
V P <vpernicka@gmail.com>
2024-10-26 12:38:40 +0200

    Refactor to propper use of mediator in get and getAll

---
+++ WebApiSample/BLL/Exceptions/ModelNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiSample.BLL.Exceptions
+{
+    public class ModelNotFoundException : Exception
+    {
+        public ModelNotFoundException(string name, object key) :
+            base($"Not found key {key} for entity {name}")
+        {
+        }
+    }
+}
